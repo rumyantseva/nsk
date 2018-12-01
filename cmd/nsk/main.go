ad90389a747f3559ec2f1b665c7ecf7a6e689aa3
Elena Grahovac <elena@grahovac.me>
2018-12-01 10:54:56 +0100

    Added simplest server

--- cmd/nsk/main.go
+++ cmd/nsk/main.go
@@ -1,9 +1,23 @@
 package main
 
-import "log"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
 	log.Print("Starting application...")
+
+	port := "8080"
+
+	s := http.Server{
+		Addr:    ":" + port,
+		Handler: nil,
+	}
+	err := s.ListenAndServe()
+	if err != nil {
+		log.Fatalf("Serever is stoped with error: %v", err)
+	}
 
 	log.Print("Application has been stoped")
 }
